7b6815dd02f47058909f953e5cf284f8309dda27
lufangjiannus@gmail.com <lufangjiannus@gmail.com@2da8f51f-8b16-d8e9-6094-5f7ee4cfe0d5>
2013-04-07 13:46:26 +0000

    Added Constants file.

---
+++ src/main/java/com/nus/cs/util/Constants.java
@@ -0,0 +1,10 @@
+package com.nus.cs.util;
+
+public class Constants {
+    
+    public static final String BUFFER_CACHED = "2000";
+    public static final String MEMORY_AREA = "2001";
+    public static final String REDO_LOG = "2002";
+    public static final String SHARED_POOL = "2114";
+
+}
