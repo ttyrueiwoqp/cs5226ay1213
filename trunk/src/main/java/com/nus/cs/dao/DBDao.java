913284dcb2e0737469e94abc5c363015bfe137e5
lufangjiannus@gmail.com <lufangjiannus@gmail.com@2da8f51f-8b16-d8e9-6094-5f7ee4cfe0d5>
2013-04-07 04:04:48 +0000

    Move sql from Service to Dao.

--- trunk/src/main/java/com/nus/cs/dao/DBDao.java
+++ trunk/src/main/java/com/nus/cs/dao/DBDao.java
@@ -2,19 +2,25 @@ package com.nus.cs.dao;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Properties;
 
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.support.JdbcDaoSupport;
 
 import com.nus.cs.dao.support.SomeRowMapper;
+import com.nus.cs.domain.DBTO;
 import com.nus.cs.domain.SomeTO;
 
 public class DBDao extends JdbcDaoSupport {
+	
+	static public Map<String, Integer> threshold = new HashMap<String, Integer>();
 
 	public Connection createConnection() throws SQLException, InstantiationException, IllegalAccessException, ClassNotFoundException {
 		
@@ -33,5 +39,85 @@ public class DBDao extends JdbcDaoSupport {
 		conn = DriverManager.getConnection(connectionURL, conProps);
 		
 		return conn;		
+	}
+
+	public DBTO getData(Connection conn, String table, String startTime, String endTime) throws SQLException {
+
+		String sql = "SELECT metric_id, metric_name, avg(average) value "
+				+ "FROM  dba_hist_sysmetric_summary WHERE metric_id =? "
+				+ "and end_time > to_date(?, 'dd/mm/yyyy HH24:MI:SS') "
+				+ "and end_time < to_date(?, 'dd/mm/yyyy HH24:MI:SS') "
+				+ "GROUP BY metric_id, metric_name";
+
+		PreparedStatement ps = conn.prepareStatement(sql);
+		ps.setString(1, table);
+		ps.setString(2, startTime);
+		ps.setString(3, endTime);
+
+		ResultSet rs = ps.executeQuery();
+
+		DBTO dbTO = new DBTO();
+		if (rs.next()) {
+			dbTO.setAvgValue(rs.getDouble("value"));
+			dbTO.setStartTime(startTime);
+			dbTO.setEndTime(endTime);
+		}
+		
+		ps.close();
+		
+		return dbTO;
+	}
+
+	public void setThreshold(Connection conn) throws SQLException {
+		
+		String sql = " DROP TABLE THRESHOLD_CONFIG ";
+		PreparedStatement ps = conn.prepareStatement(sql);
+		ps.execute();
+		
+		sql = " CREATE TABLE THRESHOLD_CONFIG ( PARAM_NAME VARCHAR2(100 BYTE), PARAM_WARNING NUMBER(10, 2), PARAM_CRITICAL NUMBER(10, 2) )";
+		ps = conn.prepareStatement(sql);
+		ps.execute();
+		
+		sql = " insert into THRESHOLD_CONFIG(PARAM_NAME, PARAM_WARNING, PARAM_CRITICAL) values ('Shared Pool',75,85) ";
+		ps = conn.prepareStatement(sql);
+		ps.execute();
+		
+		sql = " insert into THRESHOLD_CONFIG(PARAM_NAME, PARAM_WARNING, PARAM_CRITICAL) values ('Buffer Cache',65,80) ";
+		ps = conn.prepareStatement(sql);
+		ps.execute();
+		
+		sql = " insert into THRESHOLD_CONFIG(PARAM_NAME, PARAM_WARNING, PARAM_CRITICAL) values ('Redo Log Buffer',65,90) ";
+		ps = conn.prepareStatement(sql);
+		ps.execute();
+		
+		sql = " insert into THRESHOLD_CONFIG(PARAM_NAME, PARAM_WARNING, PARAM_CRITICAL) values ('Memory Area Used For Sorting',60,85) ";
+		ps = conn.prepareStatement(sql);
+		ps.execute();
+		
+		sql = " insert into THRESHOLD_CONFIG(PARAM_NAME, PARAM_WARNING, PARAM_CRITICAL) values ('Redo Log Files',1000,9000) ";
+		ps = conn.prepareStatement(sql);
+		ps.execute();
+		
+		ps.close();
+		
+	}
+
+	public void getThreshold(Connection conn) throws SQLException {
+		
+		String sql = " SELECT param_name, param_warning, param_critical from threshold_config ";
+
+		PreparedStatement ps = conn.prepareStatement(sql);
+
+		ResultSet rs = ps.executeQuery();
+
+		while (rs.next()) {
+			threshold.put(rs.getString("PARAM_NAME") + " Warning",
+					rs.getInt("param_warning"));
+			threshold.put(rs.getString("PARAM_NAME") + " Critical",
+					rs.getInt("param_critical"));
+		}
+
+		ps.close();
+		
 	}
 }
